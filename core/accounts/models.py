3a9d9061d9e3838f161aab185dfa3783fc4a3695
codewuo <63615179+saeedwuo@users.noreply.github.com>
2025-08-23 01:19:52 +0330

    ad

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password,**ectra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    
+    
+    def create_superuser(self,email,password,**extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        
+        extra_fields.setdefault("is_staff", True)
+        
+        extra_fields.setdefault("is_superuser", True)
+        
+        extra_fields.setdefault("is_active", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """
+    custom User Model for our app
+    """
+    email = models.EmailField(max_length=255,unique=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    
+    def __str__(self):
+        return self.email
+    
--- core/core/settings.py
+++ core/core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'accounts',
+    # 'blog'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+AUTH_USER_MODEL = "accounts.User"
+
