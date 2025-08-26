19f1c022b7b9668faf1bcbb2f57181fce676d66f
codewuo <63615179+saeedwuo@users.noreply.github.com>
2025-08-26 23:31:55 +0330

    add

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,28 +5,64 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email','is_superuser','is_active')
-    list_filter = ('email','is_superuser','is_active')
-    search_fields = ('email', )
-    ordering = ('email',)
+    list_display = ("email", "is_superuser", "is_active")
+    list_filter = ("email", "is_superuser", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
     fieldsets = (
-        ('Authentication', {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
-    )
-    
-    
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": (
-                "email", "password","password2", "is_staff",
-                "is_active", "is_superuser"
-            )}
+        (
+            "Authentication",
+            {
+                "fields": (
+                    "email",
+                    "password"
+                )
+            }
+        ),
+        (
+            "Permissions",
+            {"fields": (
+                "is_staff",
+                "is_active", 
+                "is_superuser"
+                )
+            }
+        ),
+        (
+            "group permissions",
+            {
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "important date",
+            {
+                "fields":(
+                    "last_login",
+                )
+            }
         ),
     )
-    
-    
-    
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
 
 
-admin.site.register(User,CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
